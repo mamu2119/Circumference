e9f48b0810d53339c3b265f9f1c2060b630a12f4
mamu2119 <chaitanyapandya1985@gmail.com>
2025-04-14 15:22:17 +0530

    Add files via upload

---
+++ Operations in python.py
@@ -0,0 +1,9 @@
+Tree1 = 90
+Tree2 = 54
+Tree3 = 95
+Tree4 = 59
+Tree5 = 9956
+sum = Tree1+Tree2+Tree3+Tree4+Tree5
+print("The sum of all the trees are: ", sum)
+average = sum/5
+print("The average of all trees are: ",average)
